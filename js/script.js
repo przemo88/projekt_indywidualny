627d492711a8ec1315496c3ea047807220318acc
Przemyslaw Przewoznik <blagoff88@gmail.com>
2018-12-04 00:49:04 +0100

    fixed desktop menu

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ function toggleMenu(visible) {
   document.querySelector('.top').classList.toggle('hidden-top'); 
   document.querySelector('.top').classList.toggle('menu-list-hidden');
   document.querySelector('.content').classList.toggle('col-l-12');
-  document.querySelector('.menu-list').classList.toggle('menu-list-hidden');
+  document.querySelector('.menu-list').classList.toggle('show');
+  document.querySelector('.photo').classList.toggle('show');
+  //document.querySelector('.menu').classList.toggle('col-s-12');
 }
 
 document.querySelector('.hamburger').addEventListener('click', function(e) {
